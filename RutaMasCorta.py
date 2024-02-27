b90566a0cf93e46acb86833b45dd9781485a114c
MeliLiz <melifernandez@ciencias.unam.mx>
2024-02-27 17:09:51 -0600

    Comentarios

--- RutaMasCorta.py
+++ RutaMasCorta.py
@@ -1,7 +1,7 @@
 import random as r
 import re
 
-#Fase adivinadora
+# Fase adivinadora: Seleccionar las aristas que formarán parte de la trayectoria usando random
 def fase_adivinadora(aristas):
     elegidas = []
     for arista in aristas:
@@ -10,19 +10,22 @@ def fase_adivinadora(aristas):
             elegidas.append(arista)
     return elegidas
 
-#Fase verificadora
+#Fase verificadora: Verificar su la suma de los pesos de las aristas elegidas es menor o igual a k, 
+# y si es una u-v trayectoria con las aristas elegidas
+# Recibe: Lista de aristas de la forma [u, v, peso], int k, int vértice inicial, int vértice final, int numero de vertices
+# Regresa: True si es una u-v trayectoria de peso < = k, False en otro caso
 def fase_verificadora(elegidas, k, u_inicial, v_final, num_vertices):
     
-    suma = sumar_pesos(elegidas)
+    suma = sumar_pesos(elegidas) # Obtenemos la suma de los pesos de las aristas
         
-    if suma > k: 
+    if suma > k: # Si la suma es mayor a k, no es lo que buscamos
         print("La suma de los pesos de las aristas elegidas es mayor a k")
         return False
     else: #Verificar si es una u-v trayectoria
         
-        matriz = crear_matriz_adyacencias(elegidas, num_vertices)
+        matriz = crear_matriz_adyacencias(elegidas, num_vertices) # Crear matriz de adyacencias de acuerdo a las aristas elegidas
         
-        #Verificar Si alguna arista tiene a u
+        #Verificar Si alguna arista incide en u_inicial
         inicial = False
         for num in matriz[u_inicial]:
             if num != 0:
@@ -30,7 +33,7 @@ def fase_verificadora(elegidas, k, u_inicial, v_final, num_vertices):
                 break
          
         if inicial:
-            #Verificar Si alguna arista tiene a v
+            #Verificar Si alguna arista incide en v_final
             final = False 
             for num in matriz[v_final]:
                 if num != 0:
@@ -39,7 +42,7 @@ def fase_verificadora(elegidas, k, u_inicial, v_final, num_vertices):
             
             if final:
                 
-                matriz = dfs(matriz, u_inicial)
+                matriz = dfs(matriz, u_inicial) # Hacemos dfs a partir del vértice inicial
                 
                 #Obtener los vértices en los que incide alguna arista elegida
                 vertices_elegidos = obtener_vertices(elegidas)
@@ -59,6 +62,9 @@ def fase_verificadora(elegidas, k, u_inicial, v_final, num_vertices):
             print("No hay aristas que incidan en el vértice inicial")
             return False 
         
+# Obtener los vertices en los que inciden las aristas
+# Recibe: Lista de aristas de la forma [u, v, peso]
+# Regresa: Lista de vertices
 def obtener_vertices(aristas):
     vertices = []
     for arista in aristas:
@@ -68,20 +74,28 @@ def obtener_vertices(aristas):
             vertices.append(arista[1])
     return vertices
        
+# Sumar los pesos de las aristas
+# Recibe: Lista de aristas de la forma [u, v, peso]
+# Regresa: Suma de los pesos de las aristas
 def sumar_pesos(aristas):
     suma = 0
     for arista in aristas:
         suma += arista[2]
     return suma
     
+# Crear matriz de adyacencias
+# Recibe: Lista de aristas de la forma [u, v, peso], int numero de vertices
+# Regresa: Matriz de adyacencias con la fila 0 y columna 0 vacías
 def crear_matriz_adyacencias(aristas, num_vertices):
-    matriz = [] #Inicializar matriz
+    #Inicializar matriz
+    matriz = [] 
     for i in range(num_vertices + 1):
         fila = []
         for j in range(num_vertices + 1):
             fila.append(0)
         matriz.append(fila)
-        
+    
+    #Hacemos las adyacencias con el peso de la arista  
     for arista in aristas:
         u = arista[0]
         v = arista[1]
@@ -90,6 +104,9 @@ def crear_matriz_adyacencias(aristas, num_vertices):
         
     return matriz
 
+# Realizar un recorrido en profundidad
+# Recibe: Matriz de adyacencias, int vertice desde donde empieza
+# Regresa: Matriz de adyacencias con los vértices visitados marcados con -1 en la fila 0 y columna 0
 def dfs(matriz_adyacencias, vertice):
     
     if matriz_adyacencias[0][vertice] == 0:
@@ -107,6 +124,8 @@ def dfs(matriz_adyacencias, vertice):
             
     return matriz_adyacencias
 
+# Imprimir matriz
+# Recibe: Matriz de adyacencias
 def imprimir_matriz(matriz):
     inicio =[]
     for i in range(0, len(matriz)):
